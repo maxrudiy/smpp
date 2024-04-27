a69db2e752290b8171efcba1547156b1b5c2af6d
Max <rudiywork@gmail.com>
2024-04-27 13:18:19 +0300

    Telegram update

--- server/services/telegram-service.js
+++ server/services/telegram-service.js
@@ -1,47 +1,40 @@
-import TelegramBot from "node-telegram-bot-api";
 import ControllerService from "./controller-service.js";
 import MessageModel from "../models/message-model.js";
+import { Telegraf, Markup, session } from "telegraf";
+import { message } from "telegraf/filters";
 
 const TELEGRAM_BOT_API_KEY = process.env.TELEGRAM_BOT_API_KEY;
 
 class TelegramService {
   async bot() {
-    const bot = new TelegramBot(TELEGRAM_BOT_API_KEY, { polling: { interval: 1000, autoStart: true } });
+    const bot = new Telegraf(TELEGRAM_BOT_API_KEY);
+    bot.use(session());
 
-    bot.on("polling_error", (err) => console.log(err));
-
-    const optKeyboard = {
-      parse_mode: "Markdown",
-      reply_markup: {
-        one_time_keyboard: true,
-        resize_keyboard: true,
-        keyboard: [
-          [
-            {
-              text: "Надати мій телефон",
-              request_contact: true,
-            },
-          ],
-        ],
-      },
-    };
-
-    const optRemove = { reply_markup: { remove_keyboard: true } };
-
-    bot.on("text", async (msg) => {
-      bot.sendMessage(msg.chat.id, "Як ми можемо зв'язатися з Вами?", optKeyboard).then(() => {
-        bot.once("contact", async (msg) => {
-          await MessageModel.create({
-            sourceAddr: msg.contact.phone_number,
-            message: `Telegram: ${msg.contact.first_name} ${msg.contact.last_name}`,
-          });
-          console.log("Open");
-          const result = await ControllerService.openDoor();
-          console.log(result);
-          bot.sendMessage(msg.chat.id, "Двері відкрито", optRemove);
-        });
-      });
+    bot.command("open", (ctx) => {
+      return ctx.reply(
+        "Залиште свій номер телефону",
+        Markup.keyboard([Markup.button.contactRequest("Надати мій телефон")]).resize()
+      );
     });
+
+    bot.on(message("contact"), async (ctx) => {
+      const contact = ctx.message.contact.phone_number;
+      const firstName = ctx.message.contact.first_name;
+      const lastName = ctx.message.contact.last_name;
+      const userId = ctx.message.contact.user_id;
+      await MessageModel.create({
+        sourceAddr: contact,
+        message: `Telegram: ${firstName} ${lastName} user_id: ${userId}`,
+      });
+      const result = await ControllerService.openDoor();
+      console.log(result);
+      await ctx.reply("Двері відкрито", Markup.removeKeyboard());
+    });
+
+    bot.launch();
+
+    process.once("SIGINT", () => bot.stop("SIGINT"));
+    process.once("SIGTERM", () => bot.stop("SIGTERM"));
   }
 }
 
