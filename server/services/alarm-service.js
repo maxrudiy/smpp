d840c014f3cb0ad10fa71a6ae603b41d39145efc
Max <rudiywork@gmail.com>
2024-04-28 02:24:31 +0300

    Update

--- server/services/alarm-service.js
+++ server/services/alarm-service.js
@@ -1,6 +1,18 @@
 import AlarmModel from "../models/alarm-model.js";
 
 const REGION = process.env.REGION;
+const ALARM_URL = process.env.ALARM_URL;
+
+const fetchAlarms = async () => {
+  try {
+    const response = await fetch(ALARM_URL);
+    if (response && response.status === 200) {
+      return await response.json();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 class AlarmService {
   async getAlarms() {
@@ -9,8 +21,15 @@ class AlarmService {
       await AlarmModel.create({ ongoingAlarm: false, region: REGION });
     }
 
-    const result = await AlarmModel.findOneAndUpdate({ region: REGION }, { ongoingAlarm: false }, { new: true });
-    console.log(result);
+    setInterval(async () => {
+      const data = await fetchAlarms();
+      const result = await AlarmModel.findOneAndUpdate(
+        { region: REGION },
+        { ongoingAlarm: data.states[REGION].alertnow },
+        { new: true }
+      );
+      console.log(result);
+    }, 5000);
   }
 }
 
